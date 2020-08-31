5cf3eb4c9bb515e425ccbd9a5fafe35ef4063232
Kurán Bertalan <kberci99@hotmail.hu>
2020-08-31 19:13:51 +0200

    Fixes

--- Pygame/Space Shooter/venv/Scripts/activate.bat
+++ Pygame/Space Shooter/venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=F:\Python Projects\Python Projects\Space Shooter\venv
+set VIRTUAL_ENV=E:\Gits\Python-Projects\Pygame\Space Shooter\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
